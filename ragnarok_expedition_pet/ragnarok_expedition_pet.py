70b1b633c398592c64b746fa44964a230e701f61
Takuro Huang <takuro1026@gmail.com>
2023-04-18 10:03:42 +0800

    Init commit

---
+++ ragnarok_expedition_pet/ragnarok_expedition_pet.py
@@ -0,0 +1,38 @@
+"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
+from pcconfig import config
+
+import pynecone as pc
+
+docs_url = "https://pynecone.io/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class ExampleState(pc.State):
+    # The colors to cycle through.
+    colors = ["black", "red", "green", "blue", "purple"]
+
+    # The index of the current color.
+    index = 0
+
+    def next_color(self):
+        """Cycle to the next color."""
+        self.index = (self.index + 1) % len(self.colors)
+
+    @pc.var
+    def color(self):
+        return self.colors[self.index]
+
+
+def index():
+    return pc.heading(
+        "Welcome to Pynecone!",
+        on_click=ExampleState.next_color,
+        color=ExampleState.color,
+        _hover={"cursor": "pointer"},
+    )
+
+
+# Add state and page to the app.
+app = pc.App(state=ExampleState)
+app.add_page(index)
+app.compile()
