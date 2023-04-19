bae1bd830e96b67abed067562ab931254ec9cfb4
Takuro Huang <takuro1026@gmail.com>
2023-04-19 15:54:04 +0800

    Add Dockerfile and remove unused page

--- ragnarok_expedition_pet/ragnarok_expedition_pet.py
+++ ragnarok_expedition_pet/ragnarok_expedition_pet.py
@@ -6,14 +6,9 @@ from ragnarok_expedition_pet.pages.index import index
 from ragnarok_expedition_pet.states.basic_state import BasicState
 
 
-def custom():
-    return pc.text("Custom Route")
-
-
 # Add state and page to the app.
 app = pc.App(state=BasicState)
 app.add_page(index, title="寵物最佳配對")
 app.add_page(fusion, title="合成表查詢")
-app.add_page(custom, route="/custom-route")
 app.compile()
 
