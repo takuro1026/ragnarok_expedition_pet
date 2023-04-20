e36ea0e50a1be20e65042c000bf3eff2bcd1687b
Takuro Huang <takuro1026@gmail.com>
2023-04-20 19:59:01 +0800

    Update pcconfig

--- pcconfig.py
+++ pcconfig.py
@@ -8,8 +8,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="app",
-    api_url="http://0.0.0.0:8000",
-    bun_path="/app/.bun/bin/bun",
+    port=80,
+    api_url="http://localhost:8000",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV
 )
