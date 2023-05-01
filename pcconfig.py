1970021913851f9e754ffdf4f915dacd92721214
Takuro Huang <takuro1026@gmail.com>
2023-05-01 17:28:59 +0000

    Update config for production

--- pcconfig.py
+++ pcconfig.py
@@ -8,8 +8,9 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="app",
-    port=80,
-    api_url="http://localhost:8000",
+    port=3000,
+    backend_port=5566,
+    api_url="https://ro-monster-arena-backend.takurohuang.com",
     bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
 )
